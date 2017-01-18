664b2c995d4a3da16df86e6f97f33eb22be9a7c9
Christian Lemieux <clemieux@nextjump.com>
2017-01-18 15:04:53 -0500

    filtering implemented

--- static/js/home.js
+++ static/js/home.js
@@ -1,11 +1,28 @@
 $(function () {
 	$(window).load(function() {
-		tags = [];
+		tags = ["All"];
 		$("input.tag").each(function() {
-			console.log($(this).val());
 			if($.inArray($(this).val(), tags) === -1) {
 				tags.push($(this).val());
 			}
 		});
+		container = $("#filter-container");
+		$.each(tags, function(idx, value){
+			var btn = $('<button/>', {
+				text: value,
+				class: "btn",
+				click: function () { filterChampions(value); }
+			})
+			container.append(btn);
+		});
+
+		function filterChampions(val){
+			if(val == "All"){
+				$("div.champion").show();
+			}else{
+				$("div.champion").hide();
+				$("div.champion."+val).show();
+			}
+		}
 	});
 });
