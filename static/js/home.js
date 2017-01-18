875c315d6d1d30d360bcc2a05afa4c03859e18dc
Christian Lemieux <clemieux@nextjump.com>
2017-01-18 11:49:40 -0500

    pushing filtering for Sam to work on

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,11 @@
 $(function () {
-
+	$(window).load(function() {
+		tags = [];
+		$("input.tag").each(function() {
+			console.log($(this).val());
+			if($.inArray($(this).val(), tags) === -1) {
+				tags.push($(this).val());
+			}
+		});
+	});
 });
