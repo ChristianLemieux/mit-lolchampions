6b96a64f28e16313fa8227d6d8f2c3eef943c99d
sskinner <sskinner@nextjump.com>
2017-01-19 10:11:11 -0500

    go fmt on objects

--- objects/champion.go
+++ objects/champion.go
@@ -2,15 +2,15 @@ package objects
 
 type (
 	Champion struct {
-		Id       	string
-		Name      	string
-		Title   	string
-		Blurb       string
-		Attack      string
-		Defense   	string
-		Magic    	string
-		Difficulty	string
-		Image   	string
-		Tag   		string
+		Id         string
+		Name       string
+		Title      string
+		Blurb      string
+		Attack     string
+		Defense    string
+		Magic      string
+		Difficulty string
+		Image      string
+		Tag        string
 	}
 )
